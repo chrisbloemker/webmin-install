ce9c28979219776bf54b4ea919d3bca6ac83921b
Chris Bloemker <chrisbloemker@gmail.com>
2018-08-25 13:49:00 -0400

    Create webmin-install.sh

---
+++ webmin-install.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+echo "deb https://download.webmin.com/download/repository sarge contrib" >> /etc/apt/sources.list
+wget http://www.webmin.com/jcameron-key.asc
+apt-key add jcameron-key.asc
+apt install apt-transport-https -y
+apt update
+apt install webmin -y
+rm jcameron-key.asc
+echo "Webmin device has been setup successfully!"
+sleep 5
